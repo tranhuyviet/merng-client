054ce69ab194da085906e6c1be1e177ae8cff2f8
Viet Tran <tranhuyviet@gmail.com>
2020-05-26 20:47:16 +0300

    changed prod uri(heroku)

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,8 +7,8 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000',
-    // uri: 'https://merng-server-graphql.herokuapp.com/',
+    // uri: 'http://localhost:5000',
+    uri: 'https://merng-server-graphql.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
